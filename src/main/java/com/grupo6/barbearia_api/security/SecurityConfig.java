c8f6d7e17dd090deaa24946350c6e741a7dc1437
Lucas José Gomes Oliveira <lucas.jose@edu.unifil.br>
2026-03-17 00:11:04 -0300

    refactor: forçando a reestruturação de pacotes

--- src/main/java/com/grupo6/barbearia_api/security/SecurityConfig.java
+++ src/main/java/com/grupo6/barbearia_api/security/SecurityConfig.java
@@ -43,5 +43,7 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+
+        //atualizacao
     }
 }
