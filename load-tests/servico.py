c09a3cfb2609deaf238b170b1c0f0472568acd51
Fernando Junior <168040341+FernandoJR07@users.noreply.github.com>
2026-03-23 03:25:23 -0300

    feat: Teste de carga do modelo Servico

--- load-tests/servico.py
+++ load-tests/servico.py
@@ -5,7 +5,7 @@ class ServicoUser(HttpUser):
 
     @task
     def testar_servicos(self):
-        headers = {'Authorization': 'Bearer SEU_TOKEN_AQUI'}
+        headers = {'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Imo5Tmx5VnVkdzNVaEZaMml5X3o2WSJ9.eyJpc3MiOiJodHRwczovL2Rldi1tZ2JlMXNiOHo2Y3ZpNWh5LnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJMNzZ6UG9US3RORXpSVzY4clg0ZVJZRGxXMERRUGJjRkBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9hcGkuYmFyYmVhcmlhLmNvbSIsImlhdCI6MTc3NDI0MTgxNCwiZXhwIjoxNzc0MzI4MjE0LCJzY29wZSI6ImFkbWluIGZ1bmNpb25hcmlvIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIiwiYXpwIjoiTDc2elBvVEt0TkV6Ulc2OHJYNGVSWURsVzBEUVBiY0YiLCJwZXJtaXNzaW9ucyI6WyJhZG1pbiIsImZ1bmNpb25hcmlvIl19.k611Ojss876B1NASvUlDbgfWV5dd7H__yi5CWl9VV8l96Cvv_k9nTsMo3C-NleNanUgEq7OrC_hIDPtOZak0yUTa0kLYamfGQLQOvT3vrX3rUaDqM6QIxz5o6pxRTNKAc9Pvt2U32StqmwF554CVTEoz_VVZrrUFZSWtEQmvFVaGhTuPCsVv0w2lRdojp7PllTNxqf0Tm_zkFmD614wHjCQhOJijvrXFfhTSjitgk7XhIuNJuevGO9JqDON3aHEvpqDeQFcWADj9yediXSTJm7yz-XBj9EbOTyT-YeOAT2BUcPEFLi_H_in_c3nz7Zbhu9gE6mX66LMs1qjVy_vIng'}
 
         payload = {
             "nome": "Corte Moderno",
